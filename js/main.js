7373dabe0e268981318faa0f57a5ee06ea5a0953
Daniella <delgadodaniellan@gmail.com>
2019-02-13 01:12:07 -0500

    Bootstrap Change

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Daniella Delgado.", "A Designer.", "An Architect.", "A Developer.", "A Creator."],
       typeSpeed: 100,
       loop: true,
     });
